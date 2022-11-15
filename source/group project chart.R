84ee93c180d3a0bbc449e547e5adb7e25b8ce82f
Daphni <daphng@uw.edu>
2022-11-15 01:08:49 -0800

    final

--- source/group project chart.R
+++ source/group project chart.R
@@ -2,20 +2,22 @@ library(dplyr)
 
 library(ggplot2)
 
-cost_of_living <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/New_final_dataset%20-%20Sheet1.csv")
-View(cost_of_living)
+#cost_of_living <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/New_final_dataset%20-%20Sheet1.csv")
+#View(cost_of_living)
 
-df_cost_of_living <- select (final_dataset, 1,6,9)
-View(df_cost_of_living)
+
+cost_of_living <- function(data) {
+df_cost_of_living <- select (data, 1,6,9)
+#View(df_cost_of_living)
 
 
 scatterplot <- ggplot(data=df_cost_of_living, aes(x=Groceries.Index, y=Cost.of.Living.Index))+
   geom_point()
 
 scatterplot + labs(title = "                                Cost of Food in North and South America")
+}
 
-
-View(scatterplot)
+#View(scatterplot)
 
 
 
