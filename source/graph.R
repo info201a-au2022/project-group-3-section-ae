b65c58d4721c7fa3a0564b423425d28c0e634998
Daphni <daphng@uw.edu>
2022-11-15 01:01:26 -0800

    more

--- source/graph.R
+++ source/graph.R
@@ -1,20 +1,17 @@
 
-final_dataset <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/New_final_dataset%20-%20Sheet1.csv")
-View(final_dataset)
 
 library(dplyr)
 library(ggplot2)
 
+crime <- function(data) {
 df_crime <- select(final_dataset, 1,4,5)
-View(df_crime)
-
-
-#bar graph
 
 my_bar_graph <- ggplot(df_crime, aes(x = Crime.Index, y = Country, fill = Safety.Index)) +
   geom_col() 
 my_bar_graph + labs(title = "Crime and Safety in North & South America") +
   theme(plot.title = element_text(face="bold", size=15)) 
 
+}
 
+#inal_dataset <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/New_final_dataset%20-%20Sheet1.csv")
 
