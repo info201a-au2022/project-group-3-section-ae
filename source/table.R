87894324d10758d43a7d5476195b263ff3e190c8
Daphni <daphng@uw.edu>
2022-11-15 22:49:29 -0800

    more

--- source/table.R
+++ source/table.R
@@ -4,7 +4,7 @@ data <- read.csv("/Users/daphnigeorge/Downloads/New_final_dataset - Sheet1.csv")
 data_p<- function(data) {
   mutate_if(data, is.numeric, round)
 data <- data %>% group_by(Health.Care.Index) 
-df <- data.frame( Health.Care.Index = data['Health.Care.Index'],Country = data["Country"], cost_of_living = data["Safety.Index"])
+df <- data.frame( Health.Care.Index = data['Health.Care.Index'],Country = data["Country"], cost_of_living = data["Cost.of.Living.Index"])
 
 #kable(df)
 }
