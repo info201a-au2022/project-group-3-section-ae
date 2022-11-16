8237704ed059072d53b6fc174d15c915e8974123
Daphni <daphng@uw.edu>
2022-11-15 17:17:29 -0800

    good

--- source/summary_info.R
+++ source/summary_info.R
@@ -1,21 +1,27 @@
+library(dplyr)
+
+
+final_dataset <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/New_final_dataset%20-%20Sheet1.csv")
 
 #Country that is the most safest to live in.
 
-Safest_country <- df_crime %>%
-  filter(Safety.Index == max(Safety.Index, na.rm = TRUE)) %>%
+Safest_country <- final_dataset %>%
+  filter(Safety.Index == max(Safety.Index)) %>%
   pull(Country)
+print(Safest_country)
 #Panama
 
 #Country that has the most crime.
-Not_most_safest_country <- df_crime %>%
-  filter(Crime.Index == max(Crime.Index, na.rm = TRUE)) %>%
-  pull(Country)
+Not_most_safest_country <- function(final_dataset) {
+  filter(final_dataset, Crime.Index == max(Crime.Index, na.rm = TRUE)) %>%
+  return(Country)
+}
 #Venezuela
  
 
 #Country has the highest ratio of cost of living and groceries.
 
-country_highest_ratio <- df_cost_of_living %>%
+country_highest_ratio <- final_dataset %>%
   mutate(ratio = Cost.of.Living.Index/Groceries.Index) %>%
   filter(ratio == max(ratio, na.rm = TRUE)) %>%
   pull(Country)
@@ -23,7 +29,7 @@ country_highest_ratio <- df_cost_of_living %>%
 
 #Country that has the lowest ratio of cost of living to groceries.
 
-country_lowest_ratio <- df_cost_of_living %>%
+country_lowest_ratio <- final_dataset %>%
   mutate(ratio = Cost.of.Living.Index/Groceries.Index) %>%
   filter(ratio == min(ratio, na.rm = TRUE)) %>%
   pull(Country)
