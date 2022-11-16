4dea8d60081ec8add7d343b963d5d899bc16afcb
Daphni <daphng@uw.edu>
2022-11-15 22:32:36 -0800

    table

---
+++ source/table.R
@@ -0,0 +1,15 @@
+library("knitr") # make sure you load the package (once per document)
+
+data <- read.csv("/Users/daphnigeorge/Downloads/New_final_dataset - Sheet1.csv")
+
+data_1 <- function(data) {
+  mutate_if(is.numeric, round)
+data <- data %>% group_by(Health.Care.Index) 
+df <- data.frame( Health.Care.Index = data['Health.Care.Index'],Country = data["Country"], cost_of_living = data["Safety.Index"])
+
+return(kable(df))
+}
+
+
+
+
