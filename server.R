d791135a5b2c78623be44c6012aa1af8869d1231
DaphniG <114948275+DaphniG@users.noreply.github.com>
2022-12-05 08:07:10 -0800

    These r the FINAL FILES

--- server.R
+++ server.R
@@ -1,24 +1,99 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(dplyr)
+library(plotly)
+library(maps)
+library(mapproj)
+#install.packages("shinythemes")
 
+final_dataset <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/New_final_dataset%20-%20Sheet1.csv")
+#View(final_dataset)
+
+#TODO
+data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/New_final_dataset%20-%20Sheet1.csv")
+world_coordinates <- map_data("world")
+world_coordinates <- rename(world_coordinates, Country = region)
+data <- inner_join(x = data, y = world_coordinates, by = "Country")
+df_crime <- select(final_dataset, 1,4,5)
 new_data <- read.csv ("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ae/main/data/Dataset%20with%20Regions%20-%20Sheet1.csv")
 
-data <- read.csv("https://countlove.org/data/data.csv")
+#View(df_crime)
 
-
-
-# Define server logic required to draw a histogram
+# Define server logic required to draw a bargraph
 shinyServer(function(input, output) {
+  output$selectCountry1 <- renderUI({
+    selectInput("country", "Choose a country:", choices = unique(df_crime$Country))
+
+  })
+  output$selectCountry <- renderUI({
+    #selectInput("Country", "Choose a Country:", choices = unique(data$Country))
+    output$value <- renderPrint({ input$slider1 })
+    
+    
+    
+    
+  })
   output$selectRegion <- renderUI({
     selectInput("region", "Choose a region:", choices = unique(new_data$region))
   })
   
   
-  #data <- data %>% 
-    #mutate(states = str_sub(Location, -2, -1)) %>% 
-    #head(500)
   
+  barGraph <- reactive({
+    plotData <- df_crime %>%
+      filter(Country %in% input$country)
+    country  = c(rep(plotData$Country, 2))
+    condition = c("crime index", "safety index")
+    value  = c(df_crime$Crime.Index[df_crime$Country==plotData$Country], df_crime$Safety.Index[df_crime$Country==plotData$Country])
+    df = data.frame(country=country, condition=condition, value=value)
+    
+    
+    ggplot(df, aes(fill=condition, y=value, x=country)) +
+      geom_col(width = 0.30, position = position_dodge(0.45), stat="identity", colour = "black") +
+      scale_fill_manual(values = c("gold2", "darkolivegreen3")) +
+      geom_text(aes(label = value), position = position_dodge(width = 0.45), vjust=2, size = 6) +
+      #scale_y_continuous(expand=expansion(mult = 0, add = 0)) +
+      theme_update() +
+      labs(x=  "   Crime Index                Safety Index",
+           y = "Values",
+           title = "Crime and Safety in North, South, & Central America",
+           caption = "This bar graph depicts crime & safety rates for countries in North and South America.") +
+      theme(plot.title = element_text(face="bold", size=20),
+            axis.title = element_text(face="bold", size = 17),
+            axis.text = element_text(size = 15))
+    #legend.title = element_text(face= "bold", size = 20)
+  })
+  
+  
+  output$barGraph <- renderPlot({
+    barGraph()
+  })
+  
+  #secondchart map
+  
+  #output$selectCountry <- renderUI({
+    #selectInput("Country", "Choose a Country:", choices = unique(data$Country))
+    #output$value <- renderPrint({ input$slider1 })
+    
+    
+  #})
+  
+  plotMap <- reactive({
+    country_selector <- data %>% 
+      filter(Health.Care.Index < input$slider1) %>%
+      pull(Country)
+    plotData <- data %>% 
+      filter(Country %in% country_selector)
+    ggplot(plotData)+geom_polygon(aes(x=long, y=lat, group = group, fill=Health.Care.Index))+coord_map() +
+      labs(title = "Healthcare Index in North & South America") +
+      theme(plot.title = element_text(face="bold", size=15)) 
+  }) 
+  
+  
+  output$countryPlot <- renderPlot({
+    plotMap()
+  })
   scatterPlot <- reactive({
     plotData <- new_data %>% 
       filter(region %in% input$region)
@@ -33,10 +108,4 @@ shinyServer(function(input, output) {
   output$regionPlot <- renderPlot({
     scatterPlot()
   })
-  
-  output$sampleText <- renderText({
-    paste("Hello! Welcome to my sample Shiny web app. This visualization shows a scatter of the number of attendees, depending
-            on the date of the protest and the state it happened in.")
-  })
-  
 })
